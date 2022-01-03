41623321d5f3d99bd2fd84c4562e48d016796504
ntalbs <ntalbsen@gmail.com>
2022-01-03 00:26:32 +0000

    Change return type to u64
    and add sol() function

--- src/p001.rs
+++ src/p001.rs
@@ -1,19 +1,23 @@
-const LIMIT: i32 = 1000;
+const LIMIT: u64 = 1000;
 
-pub fn sol_1() -> i32 {
+fn sol_1() -> u64 {
     (1..LIMIT)
         .filter(|x| x % 3 == 0 || x % 5 == 0)
         .fold(0, |a, x| a + x)
 }
 
-pub fn sol_2() -> i32 {
-    fn sum(n: i32) -> i32 {
+fn sol_2() -> u64 {
+    fn sum(n: u64) -> u64 {
         n * (n + 1) / 2
     }
-    fn sum_of_mul(n: i32, m: i32) -> i32 {
+    fn sum_of_mul(n: u64, m: u64) -> u64 {
         m * sum(n / m)
     }
 
     let n = LIMIT - 1;
     sum_of_mul(n, 3) + sum_of_mul(n, 5) - sum_of_mul(n, 15)
+}
+
+pub fn sol() -> u64 {
+    sol_2()
 }
