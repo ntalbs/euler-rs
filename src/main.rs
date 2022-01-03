7b867bd26e5821106bf06acbaf8d5956b8b63d5e
ntalbs <ntalbsen@gmail.com>
2022-01-03 00:45:26 +0000

    sol #2 and update main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
+use std::env;
+use std::process;
 use std::time::Instant;
 
 mod p001;
+mod p002;
+mod util;
 
-fn time(f: &dyn Fn()->i32) {
+fn time(f: fn() -> u64) {
     let start = Instant::now();
     let ret = f();
     println!("took {} ms: {}", start.elapsed().as_millis(), ret);
 }
+
+fn show_usage() {
+    println!("Usage: euler-rs [num]\n");
+}
+
 fn main() {
-    time(&p001::sol_1);
-    time(&p001::sol_2);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        show_usage();
+        eprintln!("Please, specify problem number.");
+        process::exit(1);
+    }
+
+    let prob_no = match args[1].parse::<u32>() {
+        Ok(n) => n,
+        Err(e) => {
+            show_usage();
+            eprintln!("Error: problem number is not valid.");
+            process::exit(1);
+        }
+    };
+
+    let sol = match prob_no {
+        1 => p001::sol,
+        2 => p002::sol,
+        _ => panic!("not implemented yet"),
+    };
+
+    time(sol);
 }
---
+++ src/p002.rs
@@ -0,0 +1,24 @@
+struct Fibonacci {
+    a: u64,
+    b: u64,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u64;
+    fn next(&mut self) -> Option<u64> {
+        let next = self.a + self.b;
+        self.a = self.b;
+        self.b = next;
+
+        Some(self.a)
+    }
+}
+
+pub fn sol() -> u64 {
+    let fibo = Fibonacci { a: 0, b: 1 };
+    let ret = fibo
+        .take_while(|x| x < &4_000_000)
+        .filter(|x| x % 2 == 0)
+        .fold(0, |a, b| a + b);
+    ret
+}
