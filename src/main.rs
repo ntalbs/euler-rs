5f4b2bbf4686cb926b579ab2dac652a4cf5520c5
ntalbs <ntalbsen@gmail.com>
2022-03-25 00:03:15 +0000

    sol #41

--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,7 @@ fn main() {
         38 => ps::p038::sol,
         39 => ps::p039::sol,
         40 => ps::p040::sol,
+        41 => ps::p041::sol,
         _ => {
             eprintln!("Solution #{} not implemented yet", prob_no);
             process::exit(1);
---
+++ src/ps/p041.rs
@@ -0,0 +1,30 @@
+use crate::util::{from_digits, is_prime};
+use itertools::Itertools;
+
+fn find_prime(digits: &[i64]) -> Option<i64> {
+    let len = digits.len();
+    digits
+        .iter()
+        .rev()
+        .copied()
+        .permutations(len)
+        .map(|ds| from_digits(&ds))
+        .find(|n| is_prime(*n))
+}
+
+pub fn sol() -> i64 {
+    let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for i in (1..=9).rev() {
+        return match find_prime(&digits[..i]) {
+            Some(n) => n,
+            None => continue,
+        }
+    }
+    panic!("Solution not found.");
+}
+
+#[test]
+fn test() {
+    assert_eq!(sol(), 7652413);
+}
